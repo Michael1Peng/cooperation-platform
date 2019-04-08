a132fdaa261f8062c9a57c1488901f62d0809510
michael <625026338@qq.com>
2019-04-08 19:50:20 +0800

    make header and footer.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,20 @@
 import {NgModule, SkipSelf, Optional} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
   imports: [
     CommonModule
   ]
