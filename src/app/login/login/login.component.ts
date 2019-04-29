e264a14a151b7dbcb16ed8b3c048b1044891914d
michael <625026338@qq.com>
2019-04-29 08:41:37 +0800

    Finish list:   1. Use FormGroup and FormBuilder to set up the form and use validators to validate it.

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit({value, valid}, event: Event) {
+    console.log(JSON.stringify(value));
+    console.log(valid);
   }
 
 }
